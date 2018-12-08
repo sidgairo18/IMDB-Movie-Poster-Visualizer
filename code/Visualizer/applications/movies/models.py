ec4572bf4314c157942e88f9aa4138fac97c898c
subramanyam <subramanyam.kmk03@gmail.com>
2018-12-09 00:07:16 +0530

    added models, genre, movie

--- code/Visualizer/applications/movies/models.py
+++ code/Visualizer/applications/movies/models.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+	name = models.CharField(max_length=200)
+
+	def serialize(cls):
+		return {
+			'id': cls.id,
+			'name': cls.name,
+		}
+
+class Movie(models.Model):
+	name = models.CharField(max_length=200)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+	year = models.IntegerField(default=2018)
+
+	def serialize(cls):
+		return {
+			'id': cls.id,
+			'genre_id': cls.genre_id,
+			'name': cls.name,
+			'year': cls.year,
+		}
