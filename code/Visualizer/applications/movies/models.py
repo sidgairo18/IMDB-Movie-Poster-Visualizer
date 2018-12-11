731ec59d228990bd85b320f01a0e74c716c1ada2
subramanyam <subramanyam.kmk03@gmail.com>
2018-12-11 10:26:12 +0530

    update models

--- code/Visualizer/applications/movies/models.py
+++ code/Visualizer/applications/movies/models.py
@@ -1,23 +1,51 @@
 from django.db import models
 
 class Genre(models.Model):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, null=True)
 
 	def serialize(cls):
 		return {
 			'id': cls.id,
-			'name': cls.name,
+			'name': cls.name
 		}
 
 class Movie(models.Model):
-	name = models.CharField(max_length=200)
-	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-	year = models.IntegerField(default=2018)
+	title = models.CharField(max_length=200, null=True)
+	image = models.CharField(max_length=200, null=True)
+	year = models.IntegerField(default=2018, null=True)
+	country = models.CharField(max_length=200, null=True)
+	director = models.CharField(max_length=200, null=True)
+	plot = models.CharField(max_length=2000, null=True)
+	rating = models.IntegerField(default=10.0, null=True)
 
 	def serialize(cls):
 		return {
 			'id': cls.id,
-			'genre_id': cls.genre_id,
-			'name': cls.name,
+			'title': cls.title,
+			'image': cls.image,
 			'year': cls.year,
+			'country': cls.country,
+			'director': cls.director,
+			'plot': cls.plot,
+			'rating': cls.rating
+		}
+
+class MovieToGenre(models.Model):
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+	def serialize(cls):
+		return {
+			'movie': cls.movie.serialize(),
+			'genre': cls.genre.serialize()
+		}
+
+class FeatureToMovie(models.Model):
+	feature = models.CharField(max_length=200, null=True)
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+	def serialize(cls):
+		return {
+			'feature': cls.feature,
+			'movie': cls.movie.serialize()
 		}
